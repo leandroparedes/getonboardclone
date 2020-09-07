1ba5d84ab5ef68f8b2d0b1335e63860ab308d6dc
Leandro Paredes <leandroparedes6@gmail.com>
2020-09-07 19:49:44 -0300

    basic company auth views

--- src/app/Applications/ForCompanies/Frontend/Views/auth/login.blade.php
+++ src/app/Applications/ForCompanies/Frontend/Views/auth/login.blade.php
@@ -1,1 +1,27 @@
-<h1>login company</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Company login</h1>
+
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form action="{{ route('companies.login') }}" method="POST">
+        @csrf
+        <input type="email" name="email" id="id-email" placeholder="company admin email">
+        <input type="password" name="password" id="id-password" placeholder="password">
+        <input type="submit" value="Login as company">
+    </form>
+</body>
+</html>
