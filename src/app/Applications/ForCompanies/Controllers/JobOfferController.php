c8bbc20199d1ffa2107a94fd23afba1c2e8b0412
Leandro Paredes <leandroparedes6@gmail.com>
2020-09-13 00:08:58 -0300

    company: group job offers crud operations

--- src/app/Applications/ForCompanies/Controllers/JobOfferController.php
+++ src/app/Applications/ForCompanies/Controllers/JobOfferController.php
@@ -2,17 +2,13 @@
 
 namespace App\Applications\ForCompanies\Controllers;
 
+use App\Applications\ForCompanies\Logic\JobOffers\HasCrudOperations;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Applications\ForCompanies\Logic\JobOffers\Store;
-use App\Applications\ForCompanies\Logic\JobOffers\Update;
-use App\Applications\ForCompanies\Logic\JobOffers\Delete;
-use App\Applications\ForCompanies\Logic\JobOffers\Lists;
 use Illuminate\Support\Facades\Auth;
 
 class JobOfferController extends Controller
 {
-    use Store, Update, Delete, Lists;
+    use HasCrudOperations;
 
     public function __construct()
     {
---
+++ src/app/Applications/ForCompanies/Logic/JobOffers/HasCrudOperations.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Applications\ForCompanies\Logic\JobOffers;
+
+trait HasCrudOperations
+{
+    use Store, Lists, Update, Delete;
+}
